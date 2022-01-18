bf7578affcb4379f11e3f2914b44b2c6b210c112
makarand burud <makarand.burud13@gmail.com>
2022-01-18 18:41:18 +0530

    'assignment'

--- TestStringFormater.java
+++ TestStringFormater.java
@@ -1,5 +1,11 @@
 package assisnment;
 
 public class TestStringFormater {
-
+	public static void main(String[] args) {
+		
+	
+	
+	System.out.println(StringFormatter.reverseString("my name is khan"));  
+    System.out.println(StringFormatter.reverseString("I am sonoo jaiswal")); 
+}
 }
